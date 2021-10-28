4e6af6d51f756756a25e6cec4cf1a24e70ebf536
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 14:28:32 +0500

    Update main.py

--- lesson5/main.py
+++ lesson5/main.py
@@ -1,4 +1,8 @@
-
 from func import *
-x = sum_range()
+print("Введите наименьшее число диапазона")
+num1 = int(input())
+print('введите наибольшее число диапазона')
+e = int(input())
+num2 = e+1
+x = sum_range(num1, num2)
 print(x)
