f1e4936637ff726b8bcfd9e76a4171489811d834
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 15:25:26 +0500

    Update func.py

--- lesson5/func.py
+++ lesson5/func.py
@@ -1,7 +1,7 @@
 def sum_range(num1, num2):
     sum = 0
     if num1 > num2:
-        for i in range(num2-1, num1+1):
+        for i in range(num2-1, num1+1): # Почему он так считает диапазон, почему не включает??
             sum = sum + i
         return sum
     else:
