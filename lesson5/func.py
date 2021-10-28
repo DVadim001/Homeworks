aef86b8c84133d8f63c32d92879023c467c025b8
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 14:29:55 +0500

    Update func.py

--- lesson5/func.py
+++ lesson5/func.py
@@ -1,21 +1,3 @@
-# V 1
-
-# def sum_range(start=int(input()), end=int(input())):
-#     e = end+1
-#     sum = 0
-#     if start > e:
-#         for i in range(e, start):
-#             sum += i
-#         return(sum)
-#     else:
-#         for i in range(start, e):
-#             sum += i
-#         return(sum)
-
-
-
-# V 2
-
 def sum_range(num1, num2):
     sum = 0
     if num1 > num2:
