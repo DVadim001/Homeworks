07920eecd7f057161f9f1bfdf86722a28226280f
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 13:42:40 +0500

    Update func(v1).py

--- lesson5/func(v1).py
+++ lesson5/func(v1).py
@@ -1,15 +1,15 @@
 def sum_range():
     print("Введите наименьшее число диапазона")
-    num1=int(input())
+    num1 = int(input())
     print('введите наибольшее число диапазона')
-    e=int(input())
+    e = int(input())
     num2 = e+1
     sum = 0
     if num1 > num2:
-        sum_reverce = sum[::-1]
         for i in range(num1, num2):
-            sum += i
-        return type(sum_reverce)
+            sum = sum[::-1]
+            sum -= i
+        return sum
     else:
         for i in range(num1, num2):
             sum += i
