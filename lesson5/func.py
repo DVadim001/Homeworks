44ee12325092827d9b05a537f0c4adedcec9ad7a
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 15:16:40 +0500

    Update func.py

--- lesson5/func.py
+++ lesson5/func.py
@@ -1,9 +1,7 @@
 def sum_range(num1, num2):
     sum = 0
     if num1 > num2:
-        sum = int()
-        sum = sum[::-1]
-        for i in range(num1, num2):
+        for i in range(num2-1, num1+1):
             sum = sum + i
         return sum
     else:
