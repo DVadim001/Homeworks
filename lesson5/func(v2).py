b9eb6e43f29804dec7203c4d3c929a11a52e3177
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-28 13:28:01 +0500

    Create func(v2).py

---
+++ lesson5/func(v2).py
@@ -0,0 +1,11 @@
+# def sum_range(start=int(input()), end=int(input())):
+#     e = end+1
+#     sum = 0
+#     if start > e:
+#         for i in range(e, start):
+#             sum += i
+#         return(sum)
+#     else:
+#         for i in range(start, e):
+#             sum += i
+#         return(sum)
