5aa0ca0ee79ef8a818e2b2ce429f2f59d3591dab
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-29 15:29:01 +0500

    Update classes.py

--- lesson6/classes.py
+++ lesson6/classes.py
@@ -1,15 +1,20 @@
 class dogs():
 
-	def __init__(self, name, poroda, age):
+	def __init__(self, name, poroda, age, NewName):
 		self.name = name
 		self.poroda = poroda
 		self.age = age
+		self.NewName = NewName
+
 
 	def sidet(self):
-		print('Пёс {} сел'.format(self.name))
+		print('Пёс породы {} возрастом {} года по кличке {} сел'.format(self.poroda, self.age, self.name))
 
 	def lejat(self):
-		print('Пёс {} лёг'.format(self.name))
+		print('Пёс породы {} возрастом {} года по кличке {} лёг'.format(self.poroda, self.age, self.name))
 
 	def day_lapu(self):
-		print('Пёс {} дал лапу'.format(self.name))
+		print('Пёс породы {} возрастом {} года по кличке {} дал лапу'.format(self.poroda, self.age, self.name))
+
+	def rename(self):
+		print('Теперь пёс по кличке {} называется {}'.format(self.name, self.NewName))
