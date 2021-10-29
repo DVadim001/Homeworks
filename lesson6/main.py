87aaf373f0426312bdd034aec840f00ffbbdfa4f
DVadim001 <92811394+DVadim001@users.noreply.github.com>
2021-10-29 15:29:34 +0500

    Update main.py

--- lesson6/main.py
+++ lesson6/main.py
@@ -1,8 +1,12 @@
 from classes import dogs
 
-pyos1 = dogs('Graf','Pitbul', '3')
+pyos1 = dogs('Graf','Pitbul', '3', 'sss')
 
 
 pyos1.sidet()
 pyos1.lejat()
 pyos1.day_lapu()
+
+setattr(pyos1, 'NewName', 'Sharik')
+
+pyos1.rename()
